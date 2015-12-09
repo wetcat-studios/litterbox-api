0c6867987f76a369f2fd3136ffc4f180d6b30cee
Andreas Göransson <ag@santiclaws.se>
2015-12-09 22:44:50 +0100

    Added pricelist route

---
+++ src/Controllers/PricelistController.php
@@ -0,0 +1,93 @@
+<?php namespace Wetcat\Litterbox\Controllers;
+
+use Wetcat\Litterbox\Requests;
+use Wetcat\Litterbox\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Validator;
+
+use Wetcat\Litterbox\Models\Order;
+use Wetcat\Litterbox\Models\Customer;
+use Wetcat\Litterbox\Models\Article;
+
+use Ramsey\Uuid\Uuid;
+
+use Wetcat\Litterbox\Auth\Roles as RoleHelper;
+
+class PricelistController extends Controller {
+
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
+  public function index(Request $request)
+  {
+   
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create()
+  {
+    //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store(Request $request)
+  {
+    
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function show(Request $request, $id)
+  {
+    
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function edit($id)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function update($id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
+
+}
--- src/routes.php
+++ src/routes.php
@@ -36,6 +36,7 @@ Route::group(['middleware' => ['cors', 'litterbox-auth']], function ()
   Route::resource('customersegments', 'Wetcat\Litterbox\Controllers\CustomerSegmentController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
   Route::resource('emails', 'Wetcat\Litterbox\Controllers\EmailController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
   Route::resource('groups', 'Wetcat\Litterbox\Controllers\GroupController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
+  Route::resource('groups.pricelists', 'Wetcat\Litterbox\Controllers\PricelistController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
   Route::resource('ingredients', 'Wetcat\Litterbox\Controllers\IngredientController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
   Route::resource('intrastat', 'Wetcat\Litterbox\Controllers\IntrastatController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
   Route::resource('manufacturers', 'Wetcat\Litterbox\Controllers\ManufacturerController', ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
